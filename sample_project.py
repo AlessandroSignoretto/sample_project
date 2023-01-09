24da9f714c593655b4a90f84879194f0866f4dad
alesigno99@gmail.com <alesigno99@gmail.com>
2023-01-09 10:39:55 +0100

    converting project to a streamlit app

--- sample_project.py
+++ sample_project.py
@@ -1,16 +1,25 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import math_tools as mt
+import streamlit as st 
 import seaborn as sns
 
+st.header('Titanic Survaivability Analysis')
+st.subheader('this is smaller')
+st.write('A **short** explanation of the project.')
+
 titanic_df = pd.read_csv('https://raw.githubusercontent.com/agconti/kaggle-titanic/master/data/train.csv')
-titanic_df.info()
 
-plt.figure(figsize=(10,6))
-titanic_df.Age.hist()
-plt.show()
+st.write('The row data:')
+st.write(titanic_df)
 
+st.subheader('Correlation matrix')
 titanic_corr = titanic_df.corr()
-plt.figure(figsize= (10,6))
-sns.heatmap(titanic_corr, annot=True)
-plt.show()
+fig, ax = plt.subplots(figsize=(10,6))
+sns.heatmap(titanic_corr, annot = True, ax=ax)
+st.write(fig)
+
+st.subheader('Plots')
+fig, ax = plt.subplots(figsize=(10,6))
+titanic_df.Age.hist()
+st.write(fig)
